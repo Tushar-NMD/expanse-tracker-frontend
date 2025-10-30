d7d84dc6caa2c2f572315aa35512975bb7ede10e
Delta student <149225920+Tushar123097@users.noreply.github.com>
2025-10-30 16:43:27 +0530

    save

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 const conf = {
-  apiBaseUrl: import.meta.env.VITE_API_BASE_URL,
+  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'https://expanse-tracker-j1mk.onrender.com',
 };
 export default conf;
